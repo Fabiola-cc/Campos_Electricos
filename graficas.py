bd07d79c2d123742b5880802f8ed92fe57c0f44c
Maria-Villafuerte <100331510+Maria-Villafuerte@users.noreply.github.com>
2023-08-24 20:16:28 -0600

    Update graficas.py

--- graficas.py
+++ graficas.py
@@ -8,12 +8,73 @@ Fabiola Contreras 22787
 María José Villafuerte 22129
 '''
 import tkinter as tk # Libreria para creacion de interfaz grafica
+import turtle
 
 def Grafica_CampoE_LineasDeCarga(x,Direccion):
-    a = 0
+    turtle.pu()
+    turtle.setposition(-200,0)
+    turtle.pd()
+    turtle.fd(50)
+    turtle.lt(90)
+    turtle.fd(200)
+    turtle.lt(90)
+    turtle.fd(50)
+    turtle.lt(90)
+    turtle.fd(200)
+    turtle.lt(90)
+    turtle.pu()
+    turtle.fd(50)
+    turtle.lt(90)
+    turtle.fd(100)
+    turtle.rt(90)
+    turtle.pd()
+    turtle.fd(x*20)
+    turtle.done()
+     # Cerrar la ventana al hacer clic
+    turtle.exitonclick()
+
     
-def Grafica_CampoE_anillo(x,Direccion):
-    a = 0
+def Grafica_CampoE_anillo(x,Direccion): 
+    turtle.penup()  # Levantar el lápiz
+    turtle.goto(0, -100)  # Mover el cursor al centro
+    turtle.pendown()  # Bajar el lápiz
+
+    # Iniciar el relleno
+    turtle.begin_fill()
+
+    # Dibujar el anillo exterior
+    turtle.circle(100)
+    turtle.end_fill()
+
+    # Posicionar para el anillo interior
+    turtle.penup()
+    turtle.left(90)
+    turtle.forward(10)
+    turtle.right(90)
+    turtle.pendown()
+
+    # Dibujar el anillo interior
+    turtle.color("black", "white")
+    turtle.begin_fill()
+    turtle.circle(90)
+    turtle.end_fill()
+    
+    #Linea x
+    turtle.penup()
+    turtle.left(90)
+    turtle.forward(80)
+    turtle.right(90)
+    turtle.pendown()
+    turtle.fd(x*60)
+    
+
+
+    # Cerrar la ventana al hacer clic
+    turtle.exitonclick()
+
 
 def Grafica_CampoE_Disco(x,Direccion):
-    a = 0
+    asd =0 
+    
+#Grafica_CampoE_LineasDeCarga(4,4)
+Grafica_CampoE_anillo(5,4)
